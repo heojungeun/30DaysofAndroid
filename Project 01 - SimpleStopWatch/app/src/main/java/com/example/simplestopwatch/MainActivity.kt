5f4be4a198673a8981dd873ac87ee011c84c9444
heojungeun <heocube@naver.com>
2020-02-24 22:50:21 +0900

    init counter (pause not working)

--- Project 01 - SimpleStopWatch/app/src/main/java/com/example/simplestopwatch/MainActivity.kt
+++ Project 01 - SimpleStopWatch/app/src/main/java/com/example/simplestopwatch/MainActivity.kt
@@ -2,11 +2,82 @@ package com.example.simplestopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.simplestopwatch.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.timer
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bind : ActivityMainBinding
+
+    private var time=0
+    private var timerTask: Timer?=null
+    private var isRunning=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bind = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
+        bind.resetbtn.setOnClickListener{
+            reset()
+        }
+
+        bind.playbtn.setOnClickListener{
+            start()
+        }
+
+        bind.pausebtn.setOnClickListener{
+            pause()
+        }
     }
+
+    fun start(){
+        timerTask = timer(period = 10){// 0.1초에 한 번씩 호출
+            time++
+            val sec = time/100
+            val milli = time%100
+            val min = sec / 60
+
+            runOnUiThread{
+                bind.seconds.text = "$sec"
+                bind.millisec.text = "$milli"
+                bind.minute.text = "$min"
+            }
+        }
+    }
+
+    fun pause(){
+        timerTask?.cancel()
+    }
+
+    fun reset(){
+        timerTask?.cancel()
+
+        if (isRunning)
+            isRunning = false
+
+        time= 0
+
+        bind.minute.text = "00"
+        bind.millisec.text = "00"
+        bind.minute.text = "00"
+    }
+
+
+//    fun onResetClick(view: View){
+//        reset()
+//    }
+//
+//    fun onPlayClick(view: View){
+//        start()
+//    }
+//
+//    fun onPauseClick(view: View){
+//        pause()
+//    }
 }
