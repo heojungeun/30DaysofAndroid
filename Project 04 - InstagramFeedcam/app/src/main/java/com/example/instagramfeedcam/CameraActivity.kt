51949fba98900f06a52c7716018303900f87e2a0
heojungeun <heocube@naver.com>
2020-03-02 23:21:45 +0900

    add animation, cameraX method

--- Project 04 - InstagramFeedcam/app/src/main/java/com/example/instagramfeedcam/CameraActivity.kt
+++ Project 04 - InstagramFeedcam/app/src/main/java/com/example/instagramfeedcam/CameraActivity.kt
@@ -1,32 +1,147 @@
 package com.example.instagramfeedcam
 
+import android.Manifest
+import android.app.Activity
+import android.app.ActivityOptions
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.graphics.Matrix
+import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
+import android.transition.Slide
+import android.util.Rational
+import android.util.Size
+import android.view.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
-import java.io.File
-import java.io.IOException
-import java.nio.file.Files.createFile
-import java.text.SimpleDateFormat
-import java.util.*
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executors
+import kotlinx.android.synthetic.main.activity_cam.*
+
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class CameraActivity : AppCompatActivity() {
 
-    val REQUEST_IMAGE_CAPTURE = 1
-    private val PERMISSION_REQUEST_CODE: Int = 101
-    private var mCurrentPhotoPath: String? = null
+
+    private val executor = Executors.newSingleThreadExecutor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            with(window) {
+                requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+                // set an slide transition
+                enterTransition = Slide().also {
+                    it.slideEdge = Gravity.END
+                }
+                exitTransition = Slide().also {
+                    it.slideEdge = Gravity.START
+                }
+            }
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cam)
 
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            view_finder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Every time the provided texture view changes, recompute layout
+        view_finder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+
+        backbtn.setOnClickListener{
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+
+        }
+
+    }
+
+    private fun startCamera(){
+        // Create configuration object for the view_finder use case
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(640, 480))
+        }.build()
 
 
+        // Build the view_finder use case
+        val preview = Preview(previewConfig)
+
+        // Every time the view_finder is updated, recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = view_finder.parent as ViewGroup
+            parent.removeView(view_finder)
+            parent.addView(view_finder, 0)
+
+            view_finder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        // Bind use cases to lifecycle
+        // If Android Studio complains about "this" being not a LifecycleOwner
+        // try rebuilding the project or updating the appcompat dependency to
+        // version 1.1.0 or higher.
+        CameraX.bindToLifecycle(this, preview)
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = view_finder.width / 2f
+        val centerY = view_finder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when(view_finder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        view_finder.setTransform(matrix)
+    }
+
+    /**
+     * Process result from permission request dialog box, has the request
+     * been granted? If yes, start Camera. Otherwise display a toast
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                view_finder.post { startCamera() }
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
 }
--- Project 04 - InstagramFeedcam/app/src/main/java/com/example/instagramfeedcam/MainActivity.kt
+++ Project 04 - InstagramFeedcam/app/src/main/java/com/example/instagramfeedcam/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.instagramfeedcam
 
+import android.app.ActivityOptions
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+import android.transition.Slide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            with(window) {
+                requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+                // set an slide transition
+                enterTransition = Slide().also {
+                    it.slideEdge = Gravity.START
+                }
+                exitTransition = Slide().also {
+                    it.slideEdge = Gravity.END
+                }
+            }
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         camerabtn.setOnClickListener{
 
             var intent = Intent(this,CameraActivity::class.java)
-            startActivityForResult(intent, 100)
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
 
         }
     }
