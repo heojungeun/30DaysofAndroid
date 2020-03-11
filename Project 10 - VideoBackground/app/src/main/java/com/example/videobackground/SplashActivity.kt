cbcb89dac7f5c0ed68dbf3418bc005b5bf185199
heojungeun <heocube@naver.com>
2020-03-11 21:45:15 +0900

    init pj10

---
+++ Project 10 - VideoBackground/app/src/main/java/com/example/videobackground/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.videobackground
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        try {
+            Thread.sleep(3000)
+        }catch (e:InterruptedException){
+            e.printStackTrace()
+        }
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
+    }
+
+}
