ae6c7b888d4d028928805b8fc9b1e0ca20db7b51
heojungeun <heocube@naver.com>
2020-03-26 21:16:26 +0900

    init pj 15

---
+++ Project 15 - TumblrMenu/app/src/main/java/com/example/tumblrmenu/MainActivity.kt
@@ -0,0 +1,1 @@
+package com.example.tumblrmenuimport androidx.appcompat.app.AppCompatActivityimport android.os.Bundleclass MainActivity : AppCompatActivity() {    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        setContentView(R.layout.activity_main)    }}
