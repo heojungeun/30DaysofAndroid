12c81219ea5a35bf6e65f7f80553237da79bb5ce
heojungeun <heocube@naver.com>
2020-03-31 19:53:00 +0900

    complete text,face,label - url load

--- Project 16 - FirstMLkit/app/src/main/java/com/example/firstmlkit/MainActivity.kt
+++ Project 16 - FirstMLkit/app/src/main/java/com/example/firstmlkit/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.firstmlkit
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import com.bumptech.glide.Glide
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -32,6 +39,78 @@ class MainActivity : AppCompatActivity() {
             val dialog = builder.create()
             dialog.show()
             dialog.window?.setBackgroundDrawableResource(R.drawable.whiteborder)
+        }
+
+        mltextbtn.setOnClickListener {
+            val textImg = FirebaseVisionImage.fromBitmap(
+                (mlimgview.drawable as BitmapDrawable).bitmap
+            )
+
+            val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+
+            detector.processImage(textImg)
+                .addOnCompleteListener {
+                    var detectedText = "* "
+                    it.result?.textBlocks?.forEach{
+                        detectedText += it.text + "\n"
+                    }
+                    runOnUiThread {
+                        imgcnt.text = detectedText
+                    }
+                }
+                .addOnFailureListener { e->
+                    // print mlcnt "i dont know"
+                }
+        } // https://live.staticflickr.com/5602/15552633902_62cafb7fa9_c.jpg
+
+        mlfacebtn.setOnClickListener {
+            val detector = FirebaseVision.getInstance().visionFaceDetector
+
+            detector.detectInImage(FirebaseVisionImage.fromBitmap(
+                    (mlimgview.drawable as BitmapDrawable).bitmap
+                )).addOnCompleteListener {
+                    var markedBitmap =
+                        (mlimgview.drawable as BitmapDrawable)
+                            .bitmap
+                            .copy(Bitmap.Config.ARGB_8888, true)
+                    val canvas = Canvas(markedBitmap)
+                    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+                    paint.color = Color.parseColor("#99D7591A")
+
+                    it.result?.forEach {
+                        canvas.drawRect(it.boundingBox, paint)
+                    }
+                    runOnUiThread {
+                        mlimgview.setImageBitmap(markedBitmap)
+                    }
+                }
+                .addOnFailureListener { e->
+                    // print mlcnt "i dont know"
+                }
+        }
+
+        mllabelbtn.setOnClickListener {
+            val labelImg = FirebaseVisionImage.fromBitmap(
+                (mlimgview.drawable as BitmapDrawable).bitmap
+            )
+
+            val detector = FirebaseVision.getInstance().getCloudImageLabeler()
+
+            detector.processImage(labelImg)
+                .addOnSuccessListener { labels ->
+                    var output = "* "
+                    for (label in labels) {
+                        if (label.confidence > 0.7)
+                            output += label.text + "\n"
+                    }
+
+                    runOnUiThread {
+                        imgcnt.text = output
+                    }
+                }
+                .addOnFailureListener { e->
+                    // print mlcnt "i dont know"
+                }
         }
 
     }
