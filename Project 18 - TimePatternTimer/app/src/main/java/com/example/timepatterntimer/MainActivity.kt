7598aef208f0b3b4ab2d6d083750a7d71c39a389
heojungeun <heocube@naver.com>
2020-04-29 17:23:59 +0900

    add seekbarchangeListener

--- Project 18 - TimePatternTimer/app/src/main/java/com/example/timepatterntimer/MainActivity.kt
+++ Project 18 - TimePatternTimer/app/src/main/java/com/example/timepatterntimer/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.timepatterntimer
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import me.tankery.lib.circularseekbar.CircularSeekBar
+import me.tankery.lib.circularseekbar.CircularSeekBar.OnCircularSeekBarChangeListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +14,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        seekbar.setOnSeekBarChangeListener(object : OnCircularSeekBarChangeListener {
 
+            var finalProgress: String = "00:00:00"
+            override fun onProgressChanged(
+                circularSeekBar: CircularSeekBar,
+                progress: Float,
+                fromUser: Boolean
+            ) {
+                var sec = progress.toInt()
+                var min = sec/60
+                var hour = min/60
+                min %= 60
+                textTime.text = String.format("%02d:%02d", hour,min)
+            }
+
+            override fun onStopTrackingTouch(seekBar: CircularSeekBar) {
+            }
+
+            override fun onStartTrackingTouch(seekBar: CircularSeekBar) {}
+        })
+
+        cancelbtn.setOnClickListener {
+            textTime.text = "00:00:00"
+        }
+
+        startbtn.setOnClickListener {
+            
+        }
 
         setupbtn.setOnClickListener {
             var intent = Intent(this, SetupActivity::class.java)
