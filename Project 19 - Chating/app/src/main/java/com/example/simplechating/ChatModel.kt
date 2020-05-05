fcfae425393bac4de1dd51a929cc1673721a09ba
heojungeun <heocube@naver.com>
2020-05-05 23:01:06 +0900

    add chatmodel, activity

---
+++ Project 19 - Chating/app/src/main/java/com/example/simplechating/ChatModel.kt
@@ -0,0 +1,3 @@
+package com.example.simplechating
+
+class ChatModel(val name: String, val script:String, val profile_img:String, val cdate:String)
--- Project 19 - Chating/app/src/main/java/com/example/simplechating/MainActivity.kt
+++ Project 19 - Chating/app/src/main/java/com/example/simplechating/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.simplechating
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var prefer: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        prefer = getSharedPreferences("USERSIGN", Context.MODE_PRIVATE)
+        val editor = prefer!!.edit()
+
+        // btn click -> input name -> save sharedpreference, intent(name)
+        transbtn.setOnClickListener {
+            editor.putString("name", main_edtTxt.text.toString())
+            val intent = Intent(this, ChatRoomActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
