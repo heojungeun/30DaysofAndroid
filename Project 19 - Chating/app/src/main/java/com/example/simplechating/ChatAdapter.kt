70e75dce115ca9e7005b0c6b8e84d329103146d8
jung <heocube@naver.com>
2020-05-07 11:55:14 +0900

    modified crActivity, add chatAdapter

---
+++ Project 19 - Chating/app/src/main/java/com/example/simplechating/ChatAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.simplechating
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+
+class ChatAdapter(val context: Context, val chatlist: ArrayList<ChatModel>)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+{
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun addItem(item: ChatModel){
+
+    }
+}
--- Project 19 - Chating/app/src/main/java/com/example/simplechating/ChatRoomActivity.kt
+++ Project 19 - Chating/app/src/main/java/com/example/simplechating/ChatRoomActivity.kt
@@ -1,14 +1,49 @@
 package com.example.simplechating
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_chatroom.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChatRoomActivity : AppCompatActivity(){
+
+    internal lateinit var preferences: SharedPreferences
+    lateinit var mAdapter: ChatAdapter
+    var chatList = arrayListOf<ChatModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatroom)
+        preferences = getSharedPreferences("USERSIGN", Context.MODE_PRIVATE)
 
-        
+        mAdapter = ChatAdapter(this, chatList)
+        chatroom_Rcview.adapter = mAdapter
+        val layoutManager = LinearLayoutManager(this)
+        chatroom_Rcview.layoutManager = layoutManager
+        chatroom_Rcview.setHasFixedSize(true)
+
+        chatroom_Sendbtn.setOnClickListener {
+
+        }
+    }
+
+    fun sendMessage(){
+        val getTime = getTodayDate()
+        val item = ChatModel(preferences.getString("name","")!!,
+            chatroom_Text.text.toString(),"example",getTime)
+        mAdapter.addItem(item)
+        mAdapter.notifyDataSetChanged()
+
+        chatroom_Text.setText("")
+    }
+
+    fun getTodayDate() : String {
+        var today = Calendar.getInstance().time
+        var formatter = SimpleDateFormat("hh:mm:ss a")
+        return formatter.format(today)
     }
 }
