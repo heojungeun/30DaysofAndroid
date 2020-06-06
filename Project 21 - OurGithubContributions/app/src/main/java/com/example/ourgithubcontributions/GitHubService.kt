1976aa74ce518b1080a88c8a19548d30fa4b208a
heojungeun <heocube@naver.com>
2020-06-06 23:10:24 +0900

    add githubService, graphAdapter

---
+++ Project 21 - OurGithubContributions/app/src/main/java/com/example/ourgithubcontributions/GitHubService.kt
@@ -0,0 +1,13 @@
+package com.example.ourgithubcontributions
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface GitHubService {
+
+//    @GET("users/{user}/repos")
+//    fun listRepos(@Path("user") user: String?): Call<List<Repo?>?>?
+//
+}
---
+++ Project 21 - OurGithubContributions/app/src/main/java/com/example/ourgithubcontributions/GraphAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.ourgithubcontributions
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GraphAdapter : RecyclerView.Adapter<GraphAdapter.ViewHolder>(){
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: GraphAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GraphAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}
