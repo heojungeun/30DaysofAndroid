c8ba5499bd9a909c5625f80b6fc4965aa04cbcfb
heojungeun <heocube@naver.com>
2020-06-15 19:02:55 +0900

    modified splash, add screenshot

--- Project 21 - OurGithubContributions/app/src/main/java/com/example/ourgithubcontributions/ui/SplashActivity.kt
+++ Project 21 - OurGithubContributions/app/src/main/java/com/example/ourgithubcontributions/ui/SplashActivity.kt
@@ -2,9 +2,11 @@ package com.example.ourgithubcontributions.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ourgithubcontributions.R
 import com.example.ourgithubcontributions.SharedPreferenceManager
+import java.lang.Exception
 
 class SplashActivity: AppCompatActivity() {
 
@@ -12,15 +14,17 @@ class SplashActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        if (SharedPreferenceManager.token == ""){
-            // 처음 앱 들어왔을때
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }else{
-            // 자동 로그인하기
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        Handler().postDelayed({
+            if (SharedPreferenceManager.token == ""){
+                // 처음 앱 들어왔을때
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }else{
+                // 자동 로그인하기
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }, 1000L)
 
     }
 
