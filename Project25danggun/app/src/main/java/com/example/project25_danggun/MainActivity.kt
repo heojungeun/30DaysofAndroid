8dd0f7468831d6ca53379c7db4ade70260e07681
heojungeun <heocube@naver.com>
2021-07-15 23:30:20 +0900

    update adapter

--- Project25danggun/app/src/main/java/com/example/project25_danggun/MainActivity.kt
+++ Project25danggun/app/src/main/java/com/example/project25_danggun/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.project25_danggun.myPage.MyPageFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- Project25danggun/app/src/main/java/com/example/project25_danggun/home/ArticleAdapter.kt
+++ Project25danggun/app/src/main/java/com/example/project25_danggun/home/ArticleAdapter.kt
@@ -1,11 +1,54 @@
 package com.example.project25_danggun.home
 
-import android.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 
-class ArticleAdapter: ListAdapter<ArticleModel , ArticleAdapter.ViewHolder>(diffUtil) {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.project25_danggun.databinding.ItemArticleBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ArticleAdapter: ListAdapter<ArticleModel, ArticleAdapter.ViewHolder>(diffUtil) {
 
     inner class ViewHolder(private val binding: ItemArticleBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(articleModel: ArticleModel){
+            val format = SimpleDateFormat("MM월 dd일")
+            val date = Date(articleModel.createdAt)
 
+            binding.titleTextView.text = articleModel.title
+            binding.dateTextView.text = format.format(date).toString()
+            binding.priceTextView.text = articleModel.price
+
+            if(articleModel.imageUrl.isNotEmpty()) {
+                Glide.with(binding.thumbnailImageView)
+                        .load(articleModel.imageUrl)
+                        .into(binding.thumbnailImageView)
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<ArticleModel>(){
+            override fun areItemsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
+                return oldItem.createdAt == newItem.createdAt
+            }
+
+            override fun areContentsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
+                return oldItem == newItem
+            }
+
+        }
     }
 }
