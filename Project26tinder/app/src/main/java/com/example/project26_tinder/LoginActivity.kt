acbd39c749acfffb01544fecfa1d48eb0feee7f3
heojungeun <heocube@naver.com>
2021-07-21 06:05:25 +0900

    add facebook login

--- Project26tinder/app/src/main/java/com/example/project26_tinder/LoginActivity.kt
+++ Project26tinder/app/src/main/java/com/example/project26_tinder/LoginActivity.kt
@@ -1,11 +1,18 @@
 package com.example.project26_tinder
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -13,16 +20,44 @@ import com.google.firebase.ktx.Firebase
 class LoginActivity: AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var callbackManager: CallbackManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = Firebase.auth
+        callbackManager = CallbackManager.Factory.create()
 
         initLoginButton()
         initSignUpButton()
         initEmailAndPasswordEditText()
+        initFacebookLoginButton()
+    }
+
+    private fun initFacebookLoginButton() {
+        val facebookLoginButton = findViewById<LoginButton>(R.id.facebookLoginButton)
+
+        facebookLoginButton.setPermissions("email", "public_profile")
+        facebookLoginButton.registerCallback(callbackManager, object: FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult) {
+                val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this@LoginActivity) {
+                        if (it.isSuccessful){
+                            finish()
+                        }else{
+                            Toast.makeText(this@LoginActivity,"failed login Facebook", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+            }
+
+            override fun onCancel() {}
+
+            override fun onError(error: FacebookException?) {
+                Toast.makeText(this@LoginActivity,"failed login Facebook", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun initLoginButton() {
@@ -83,5 +118,11 @@ class LoginActivity: AppCompatActivity() {
     }
     private fun getInputPassword(): String {
         return findViewById<EditText>(R.id.passwordEditText).text.toString()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 }
